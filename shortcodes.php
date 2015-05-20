869793878fcaff97a18da8a3050a6305f24eca35
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-05-20 13:43:14 -0400

    More shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -11,4 +11,24 @@ add_shortcode('fast_facts', function($atts){
 	</div>';
 });
 
- 
+add_shortcode('label', function($atts){
+	$atts = shortcode_atts(
+		array(
+			'label' => Label,
+		), $atts);
+		print_r($atts);
+	return '
+	<div class="container_fast_facts">
+	</div>';
+});
+
+add_shortcode('value', function($atts){
+	$atts = shortcode_atts(
+		array(
+			'value' => Value,
+		), $atts);
+		print_r($atts);
+	return '
+	<div class="container_fast_facts">
+	</div>';
+});

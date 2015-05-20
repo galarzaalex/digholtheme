d879cd297a55c1da672ea6a0a4450d97e134673a
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-05-20 12:40:06 -0400

    More Edits

--- shortcodes.php
+++ shortcodes.php
@@ -3,7 +3,7 @@
 add_shortcode('fast_facts', function($atts){
 	$atts = shortcode_atts(
 		$array (
-			'name' => 'Name Text',
+			'name' => Name Text,
 		);
 	return '
 		<div class="container_fast_facts">

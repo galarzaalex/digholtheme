2aee32d91f54241ca6a4bf00b0517f8eaac2e97f
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-05-20 10:55:40 -0400

    Shortcakes for Fast Facts

---
+++ shortcodes.php
@@ -0,0 +1,11 @@
+<?php
+
+add_shortcode('fast_facts,' function($atts){
+	$atts = shortcode_atts(
+	array(
+		'name' => 'Name'
+	), $atts);
+	return '<div class='container_fast_facts'>
+			
+		</div>';
+});

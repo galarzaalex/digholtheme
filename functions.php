0ded3afaf24ab88db4b33b994d9eabb728574cfc
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-05-20 11:09:45 -0400

    Revert "Shortcakes for Fast Facts"
    This reverts commit 2aee32d91f54241ca6a4bf00b0517f8eaac2e97f.

--- functions.php
+++ functions.php
@@ -3,5 +3,3 @@ function child_scripts_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array() );
 }
 add_action( 'wp_enqueue_scripts', 'child_scripts_styles' );
-
-include 'shortcodes.php';

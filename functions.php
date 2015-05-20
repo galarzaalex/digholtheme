da71b78af59596eefcfda6e53e33f0331f59adca
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-05-20 14:23:02 -0400

    Maybe this time

--- functions.php
+++ functions.php
@@ -8,3 +8,13 @@ add_action( 'wp_enqueue_scripts', 'child_scripts_styles' );
 function fastfacts($atts, $content = null) {
 	return '<a href="'.$to.'">'. do_shortcode($content) .'</a>';
 }
+add_shortcode('fast_facts', function($atts){
+	$atts = shortcode_atts(
+		array(
+			'name' => Name,
+		), $atts);
+		print_r($atts);
+	return '
+	<div class="container_fast_facts">
+	</div>';
+});

c3cbb1cac1e6410553c8a845fc627d30e9445f06
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-05-20 12:02:16 -0400

    Shortcodes Again

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
+include 'shortcodes.php';
+
 function child_scripts_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array() );
 }
 add_action( 'wp_enqueue_scripts', 'child_scripts_styles' );
+
---
+++ shortcodes.php
@@ -0,0 +1,13 @@
+<?php
+
+add_shortcode('fast_facts', function(){
+	return '<div class='container_fast_facts'>
+			
+			</div>';
+});
+
+add_shortcode('name', function(){
+	return '<div class='name'>
+		</div>'
+
+})

e84ef299fd3c674d6ad52a21b8391d41a0bff836
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-05-26 11:17:48 -0400

    Fix

--- functions.php
+++ functions.php
@@ -41,7 +41,7 @@ add_shortcode('content_link', function($atts){
 	$atts = shortcode_atts(
 		array(
 			'title' => 'Contents',
-			'link' => '<a href="'.$to.'">''</a>';
+			'link' => 'Link'
 		), $atts);
 		
 	return '

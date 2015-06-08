5f30bff11241ec7f11dbd59010476af6621c0033
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-06-08 17:04:13 -0400

    More changes

--- category.php
+++ category.php
@@ -37,10 +37,7 @@ function sort_function ($post1, $post2) {
 			$posts_by_tag = aggregate_posts_by_tag ();
 			$i = 0;
 			$accordion = "[accordion openfirst='true']";
-			ksort($posts_by_tag) foreach (array_keys($posts_by_tag) as $tag) {
-				$accordion .= "[accordion-item title='$tag']";
-				
-				$posts = $posts_by_tag[$tag];
+			ksort($posts_by_tag) 
 				uasort($posts, "sort_function");	
 				foreach ($posts as $a_post) {
 					$ID = $a_post->ID;

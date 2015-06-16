c1a9dc954aa9597cc33e414157c3ffbcbb73a923
aihoffman1 <hoffman.allyson.i@gmail.com>
2015-06-16 10:44:10 -0400

    New DigHol Single php

---
+++ single.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * @package Catch Themes
+ * @subpackage Catch Responsive
+ * @since Catch Responsive 1.0 
+ */
+
+get_header(); ?>
+
+	<main id="main" class="site-main" role="main">
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'content', 'single' ); ?>
+
+		<?php //catchresponsive_content_nav( 'nav-below' ); ?>
+
+		<?php 
+			/** 
+			 * catchresponsive_comment_section hook
+			 *
+			 * @hooked catchresponsive_get_comment_section - 10
+			 */
+			do_action( 'catchresponsive_comment_section' ); 
+		?>
+		
+		<?php related_posts(); ?>
+			
+		<?php the_modified_date('Updated','l, F j, Y', '<div class="lastupdate">',  '</div>'); ?>
+		
+	<?php endwhile; // end of the loop. ?>
+
+	</main><!-- #main -->
+	
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
